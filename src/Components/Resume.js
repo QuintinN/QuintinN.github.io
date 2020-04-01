29fa7bbc6a93b7c94d994b3693846d436d7e36f6
Quintin Nguyen <quintintnguyen@gmail.com>
2020-03-31 23:16:00 -0400

    images and reformat

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -5,17 +5,27 @@ class Resume extends Component {
 
     if(this.props.data){
       var schoolImage= `images/${this.props.data.schoolImage}`;
+      var appleImage= `images/${this.props.data.appleImage}`;
+      var t1cgImage= `images/${this.props.data.t1cgImage}`;
+      var muddyBranchImage= `images/${this.props.data.muddyBranchImage}`;
+      var clubZImage= `images/${this.props.data.clubZImage}`;
       var skillmessage = this.props.data.skillmessage;
+
       var education = this.props.data.education.map(function(education){
         return <div key={education.school}><h3>{education.school}</h3>
-        <p className="info">{education.degree} <span>&bull;</span><em className="date">{education.graduated}</em></p>
-        <p>{education.description}</p></div>
+        <p className="info">{education.degree}</p>
+        <p className="info">{education.degree2}</p>
+        </div>
       })
+
       var work = this.props.data.work.map(function(work){
         return <div key={work.company}><h3>{work.company}</h3>
-            <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
-            <p>{work.description}</p>
-        </div>
+            <p className="info">{work.title} <span>&bull;</span> <em className="date">{work.years}</em></p>
+              <div>                
+                {work.description1} <br></br> 
+                {work.description2} <br></br>
+                {work.description3}</div> <br></br>
+              </div>
       })
       var skills = this.props.data.skills.map(function(skills){
         var className = 'bar-expand '+skills.name.toLowerCase();
@@ -30,7 +40,7 @@ class Resume extends Component {
          <div className="three columns header-col">
             <h1><span>Education</span></h1>
               <div>
-                    <img className="school-image" src={schoolImage} alt= "My school"/>
+                <img className="school-image" src={schoolImage} alt= "My school"/>
               </div>
          </div>
 
@@ -48,6 +58,12 @@ class Resume extends Component {
 
          <div className="three columns header-col">
             <h1><span>Work</span></h1>
+            <div>
+            <img className="apple-image" src={appleImage} alt= "apple"/>
+            <img className="t1cg-image" src={t1cgImage} alt= "tier one consulting group"/>
+            <img className="clubZ-image" src={clubZImage} alt= "club z"/>
+            <img className="muddyBranch-image" src={muddyBranchImage} alt= "muddy branch alliance"/>
+            </div>
          </div>
 
          <div className="nine columns main-col">
