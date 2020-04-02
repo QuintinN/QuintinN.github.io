f413d316e03874e20661c0c8a7bcffbc0796da81
Quintin Nguyen <quintintnguyen@gmail.com>
2020-04-02 14:32:59 -0400

    Finished resume section

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -27,10 +27,15 @@ class Resume extends Component {
                 {work.description3}</div> <br></br>
               </div>
       })
-      var skills = this.props.data.skills.map(function(skills){
-        var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
-      })
+      var projects = this.props.data.projects.map(function(projects){
+        return <div key={projects.name}><h3>{projects.name}</h3>
+        <p className="info">{work.title} <span>&bull;</span> <em className="date">{work.years}</em></p>
+          <div>                
+            {work.description1} <br></br> 
+            {work.description2} <br></br>
+            {work.description3}</div> <br></br>
+          </div>
+  })
     }
 
     return (
@@ -71,23 +76,15 @@ class Resume extends Component {
         </div>
     </div>
 
-
-
       <div className="row skill">
 
          <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
+            <h1><span>Projects</span></h1>
          </div>
 
          <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
 				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
+					  {projects}
 				</div>
 			</div>
       </div>
