c514d7a01e476f69fc74325f6458a5824419746e
Quintin Nguyen <quintintnguyen@gmail.com>
2021-01-14 18:27:44 -0500

    Updates

---
+++ precache-manifest.a2c9cebc56fe81816436dd458bf1113c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860f8cce789eb9e58eb2e0175303ea78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c15de9c5a8f9b4ee58d",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f721f12ab8853fb513f5",
+    "url": "/static/js/2.a0e54d54.chunk.js"
+  },
+  {
+    "revision": "cdae2c56e6cc9b5caa43a239188b1f4d",
+    "url": "/static/js/2.a0e54d54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c15de9c5a8f9b4ee58d",
+    "url": "/static/js/main.dcac8e7c.chunk.js"
+  },
+  {
+    "revision": "51bcd251b6ffc385cef6",
+    "url": "/static/js/runtime-main.e4ee6498.js"
+  }
+]);
